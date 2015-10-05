f38a52210edfd016ef29ca539ccf952e2a4639d2
adeilhamfajri <ade.ilham.fajri13@mhs.if.its.ac.id>
2015-10-05 20:46:21 +0700

    Update main.java
    instansiasi

--- main.java
+++ main.java
@@ -3,7 +3,22 @@ import java.net.*
 import java.util.*
 
 public class Tugas_1 {
-    public class static void main(String[] args) {
-    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            // TODO code application logic here
+            byte[] buffer = new byte[1024];
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream("H:/coba.txt"));
+            bis.read(buffer);
+            System.out.println(new String(buffer));
+            bis.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(BacaFile.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BacaFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
