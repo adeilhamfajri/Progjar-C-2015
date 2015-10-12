6c6938c5cbab14e57055260c6242f7ba1fb8d9bd
SustainLessString <wishmeluck.amien@gmail.com>
2015-10-13 00:28:25 +0700

    Update v2Server.java
    Tolong ditambahin

--- v2Server.java
+++ v2Server.java
@@ -10,34 +10,36 @@ import java.util.logging.Logger;
 import java.io.File;
 
 public class SimpleServer {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         try {
             ServerSocket servsock = new ServerSocket(6666);
-            int timeout = 10000;    //10 000 ms
-            servsock.setSoTimeout(timeout);
             System.out.println("Server mau berjalan ....");
             Socket socket = servsock.accept();
             System.out.println("Server berjalan ....");
-            
             InputStream is = socket.getInputStream();
             OutputStream os = socket.getOutputStream();
             
-            String default_location = "/home/f/ff/";
-            String ope = ListofFiles(default_location);
-            
             byte[] buf = new byte[50];
-            is.read(buf);
-//            System.out.println(new String(buf));
-            //os.write("Selamat datang di serverku\r\n".getBytes());
-            //current = in.nextLine();
-            String input = new String(buf);
-            if(input == "ls") {
-                System.out.println(input);
-  //            os.write(ope.getBytes());
-                os.flush();
-            }
             
+            is.read(buf); String input = new String(buf); /* Retrieving Response "Hello Server IF" */
+            System.out.println(new String(buf)); /* Printing Response "Hello Server IF" */
+
+            os.write("Selamat datang di serverku\r\n".getBytes()); /* Sending Response "Selamat datang di serverku */
+            os.flush();
             
+            /*-------------------------------------------------------*/
+            
+            int timeout = 10000; servsock.setSoTimeout(timeout);        // Add 10s timeout
+//          while (input != "q") {
+                if(input == "ls") {
+                    String ls = ListofFiles(input);
+                    os.write(ls.getBytes());
+                }
+//          }
+            
+            /*-------------------------------------------------------*/
+
+            // Closing Connection
             os.close();
             is.close();
             socket.close();
@@ -46,17 +48,20 @@ public class SimpleServer {
             Logger.getLogger(SimpleServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-        private static String ListofFiles(String dir) {
+
+    // Function for printing the list of the current directory
+    private static String ListofFiles(String dir) {
         File f = new File(dir);
         File[] paths = f.listFiles();
-        System.out.println("");
+
         for (File path : paths) {
             if (path.isFile())
-                System.out.println("File " + path.getName());
+                dir = ("File " + path.getName());
+//          System.out.println("File " + path.getName());
             else if (path.isDirectory())
-                System.out.println("Directory " + path.getName());
+                dir = ("Directory " + path.getName());
+//          System.out.println("Directory " + path.getName());
         }
-        System.out.println("");
         return dir;
     }
 }
